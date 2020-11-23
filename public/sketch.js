ff876fb924f96507478c7b644754083fb1c71202
MaurizioCerisola <66005558+MaurizioCerisola@users.noreply.github.com>
2020-11-23 12:22:50 +0100

    Restored last node project

--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,25 @@
+let socket = io(); //we are loading socket.io
+let myColor = "white";
+
+socket.on("connect", newConnection); //note that the syntax is similar but different wrt the server
+socket.on("mouseBroadcast", drawOtherMouse);
+socket.on("color", setColor);
+
+function setColor(assignedColor){
+  myColor =assignedColor;
+}
+
+function newConnection() {
+  console.log("your id: " + socket.id);
+}
+
+function drawOtherMouse(data){
+  push();
+  fill(data.color);
+  ellipse(data.x, data.y, 10);
+  pop();
+}
+
 function preload(){
   // put preload code here
 }
@@ -5,9 +27,24 @@ function preload(){
 function setup() {
   createCanvas(windowWidth,windowHeight)
   // put setup code here
+  background("purple");
 }
 
 function draw() {
-  background("purple");
   // put drawing code here
+}
+
+function mouseMoved() {
+  push();
+  fill(myColor);
+  ellipse(mouseX, mouseY, 20);
+  pop();
+  //create the message (as an object)
+  let message = {
+    x: mouseX,
+    y: mouseY,
+    color: myColor,
+  };
+  //send the message
+  socket.emit("mouse", message);
 }
--- server.js
+++ server.js
@@ -1,10 +1,36 @@
-console.log("node is running");
-
+console.log("node is running")
 
 let express = require("express"); //laod the piece of code called "express" and place it in the variable express
-//let socket = require("socket.io");
+let socket = require("socket.io");
 let app = express(); //execute express
 let port = 3000; //standard number, this is the port we will use for our project
 let server = app.listen(port); //start the server
-
+//the 4 lines above allows you to create your local server
 app.use(express.static("public")); //we are saying to express to use the folder "public"
+let io = socket(server); //we enable the server to receive and send messages to the clients
+
+//a message is standardized and is called "event". Every event has a name and an action associated
+io.on("connection", newConnection); //when there is a message called "connection" execute a function called "newConnection"
+
+function newConnection(socket) {
+  console.log("new connection: " + socket.client.id); //_socket.client.id by default contains the ip adress of the new connection
+
+  let clientColor = getRandomColor();
+  socket.emit("color", clientColor);
+
+  socket.on("mouse", mouseMessage); //for each client, if you get any connection called "mouse", execute "mouseMessage"
+  // mouse is a customized name, we can name it how we want
+  function mouseMessage(dataReceived){
+    console.log(socket.client.id, dataReceived);
+    socket.broadcast.emit("mouseBroadcast", dataReceived); //broadcast this information to everyone except from the one who sent it
+  }
+}
+
+ function getRandomColor() {
+   var letters = "0123456789ABCDEF"; //length = 16
+   var color = "#";
+   for (var i=0; i<6; i++){
+     color += letters[Math.floor(Math.random() * 16)];
+   }
+   return color;
+ }
